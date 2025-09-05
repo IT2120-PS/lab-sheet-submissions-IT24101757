bd8b70b6736118762e101771133c28ac86b1691f
Rathnaweera O V <It24101757@my.sliit.lk>
2025-09-05 09:13:08 +0530

    Lab_Sheet_06

---
+++ Lab Sheet 06/IT24101757.R
@@ -0,0 +1,24 @@
+setwd("C:\\Users\\User\\OneDrive\\Desktop\\SLIIT Modules\\PS\\LabSheet 06")
+getwd()
+
+#01
+
+#Given: n = 50, p = 0.85, X ~ Binomial(n = 50, p = 0.85)
+#X ∼ Binomial(50,0.85)
+
+n <- 50
+p <- 0.85
+
+prob_X <- 1 - pbinom(46, size=n, prob=p)
+prob_X
+
+#02
+#Random variable(X): Number of calls received in 1hr.
+
+#Distribution of X: X ~ Poisson(12)
+
+
+lamda <- 12
+
+probability_X <- dpois(15, lamda)
+probability_X
