caa174e19406e4acc6a1b6746d55adb4707cb7ce
Rathnaweera O V <It24101757@my.sliit.lk>
2025-09-16 11:41:09 +0530

    LabSheet_07

---
+++ Lab Sheet 07/IT24101757.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\User\\OneDrive\\Desktop\\SLIIT Modules\\PS\\LabSheet 07")
+getwd()
+
+#01
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+#02
+pexp(2, rate = 1/3)
+
+#03
+1 - pnorm(130, mean = 100, sd = 15)
+
+qnorm(0.95, mean = 100, sd = 15)
